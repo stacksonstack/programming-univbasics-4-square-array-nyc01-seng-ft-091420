56efe117d403af225fa1daccbf23e16651331114
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-01 02:18:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
+numbers = [1,2,3]
+
 def square_array(array)
-  # your code here
+  counter = 0 
+  new_array[]
+  while counter <= array.length do
+    modify = array[counter]
+    modify = Math.sqrt(modify)
+    new_array.push(modify)
+    counter++ 
+  end
 end
+
+
+ 
+square_array(numbers)
